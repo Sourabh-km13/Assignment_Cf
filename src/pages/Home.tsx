9d8cf99c305ccfb897010b40793bc01b9a0eb14a
sourabh kumar <lordgk02@gmail.com>
2026-06-14 21:14:46 +0530

    Created home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,31 @@
+import { useNavigate } from "react-router";
+import Button from "../components/Button";
+
+export default function Home() {
+    const navigate = useNavigate();
+    return (
+
+        <div className="z-10 text-center pt-40">
+            <h1 className="text-5xl md:text-6xl font-bold text-white mb-4">
+                Welcome
+            </h1>
+
+            <p className="text-gray-400 text-lg mb-10">
+                Choose an option to continue
+            </p>
+
+            <div className="flex flex-col sm:flex-row gap-6 justify-center">
+                {/* Users Button */}
+                <Button onClick={() => navigate("/users")} >
+                    User
+                </Button>
+
+                {/* Forms Button */}
+                <Button gradient='bg-gradient-to-r from-violet-500 to-indigo-500'>
+                    Forms
+                </Button>
+            </div>
+        </div>
+
+    );
+}
